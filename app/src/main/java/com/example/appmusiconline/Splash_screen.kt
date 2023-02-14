2574491ea5b9b95843bfcaaeb3220b7ef5ef7d7d
PHUONG NAM <dodangtruong2002@gmail.com>
2023-02-14 19:09:12 +0700

    update project

---
+++ app/src/main/java/com/example/appmusiconline/Splash_screen.kt
@@ -0,0 +1,80 @@
+package com.example.appmusiconline
+
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+private lateinit var auth:FirebaseAuth
+private lateinit var googleSignInClient: GoogleSignInClient
+class Splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        auth= FirebaseAuth.getInstance()
+        val gson=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient=GoogleSignIn.getClient(this,gson)
+        findViewById<Button>(R.id.btn_login_google).setOnClickListener {
+            SignupInGoogle()
+        }
+
+    }
+
+    private fun SignupInGoogle() {
+        val signInIntent= googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+    private val launcher =registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {
+        result->
+        if(result.resultCode==Activity.RESULT_OK)
+        {
+            val task=GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResult(task)
+        }
+    }
+
+    private fun handleResult(task: Task<GoogleSignInAccount>) {
+         if (task.isSuccessful)
+         {
+            val account:GoogleSignInAccount=task.result
+             if(account!=null)
+
+             {
+                 updateUI(account)
+             }
+         }
+         else
+         {
+             Toast.makeText(this, "failed", Toast.LENGTH_LONG).show()
+
+         }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+       val credential=GoogleAuthProvider.getCredential(account.idToken,null)
+      auth.signInWithCredential(credential).addOnCompleteListener {
+          if (it.isSuccessful) {
+              val intent: Intent = Intent(this, MainActivity::class.java)
+              startActivity(intent)
+          } else {
+              Toast.makeText(this, "failed", Toast.LENGTH_LONG).show()
+          }
+      }
+    }
+}
